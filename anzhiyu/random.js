31cda66009f84fe86a0e8ec0cb7d222ae55dbcaa
CheerfulChen <Cheerful_Chen5@163.com>
2024-03-08 00:26:41 +0800

    Site updated: 2024-03-08 00:26:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/03/04/INT104-Tutorial 1/","2024/03/03/一些自动登录方法/","2024/03/03/对于慧湖通宿舍上网的一些拙见/","2024/03/02/属于迟然的Hello World/","2024/03/05/CPT103-Lecture 1/"];function toRandomPost(){
+var posts=["2024/03/04/INT104-Tutorial 1/","2024/03/03/一些自动登录方法/","2024/03/03/对于慧湖通宿舍上网的一些拙见/","2024/03/02/属于迟然的Hello World/","2024/03/05/CPT103-Lecture 1/","2024/03/08/CPT102-Lecture 0/","2024/03/08/EEE104-Lecture 1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
