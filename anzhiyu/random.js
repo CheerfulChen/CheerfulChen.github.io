a1cd2b8275a42e4330220aabd071a34c26b00df6
CheerfulChen <Cheerful_Chen5@163.com>
2024-05-31 10:37:04 +0800

    Site updated: 2024-05-31 10:37:03

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/03/08/CPT102-Lecture 0/","2024/03/05/CPT103-Lecture 1/","2024/03/15/CPT103-Lecture 2/","2024/03/08/EEE104-Lecture 1/","2024/03/04/INT104-Tutorial 1/","2024/03/15/INT104-Tutorial 2/","2024/03/03/一些自动登录方法/","2024/03/03/对于慧湖通宿舍上网的一些拙见/","2024/03/02/属于迟然的Hello World/","2024/05/28/EEE104-Lecture 2/"];function toRandomPost(){
+var posts=["2024/03/08/CPT102-Lecture 0/","2024/03/05/CPT103-Lecture 1/","2024/03/15/CPT103-Lecture 2/","2024/03/08/EEE104-Lecture 1/","2024/03/04/INT104-Tutorial 1/","2024/03/15/INT104-Tutorial 2/","2024/03/03/一些自动登录方法/","2024/03/03/对于慧湖通宿舍上网的一些拙见/","2024/03/02/属于迟然的Hello World/","2024/05/28/EEE104-Lecture 2/","2024/05/31/CPT102-图论/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
